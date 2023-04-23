5b2e5bdc2b569fcdb51fa0cec5590ac16f64b8bf
CarlosP <carlos.parra.at@usa.edu.co>
2023-04-22 23:26:45 -0500

    Primero

---
+++ src/main/java/com/example/demo2app/Demo2appApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo2app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2appApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2appApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo2app/Demo2appApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo2app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2appApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
